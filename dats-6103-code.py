1098db1d955580b0e9af3021dc8e627c12344050
quanyuan42 <91325210+quanyuan42@users.noreply.github.com>
2023-08-10 08:58:49 -0400

    Update dats-6103-code.py

--- dats-6103-code.py
+++ dats-6103-code.py
@@ -6,4 +6,20 @@ import matplotlib.pyplot as plt
 
 # %%
 data = pd.read_csv('202306-capitalbikeshare-tripdata.csv')
+print(len(data))
+print(data.dtypes)
+#%%
+#data.head()
+#data.shape
+data.isnull().sum()/data.shape[0]*100  # inspect missing
+# %%
+col_not_use=['start_station_name','start_station_id','end_station_name','end_station_id'] #no need cols
+data1=data.drop(col_not_use,axis=1)
+data1.head()
+
+data1 = data1.dropna() #clear missing
+data1.shape
+# %%
+data1.dtypes
+#data1.info
 # %%
